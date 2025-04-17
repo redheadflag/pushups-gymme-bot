896a49c2a3ea729849d459666a6a377e67d2b92f
smplgdev <73465772+smplgdev@users.noreply.github.com>
2025-04-18 01:08:38 +0200

    fixes

--- src/core/redis.py
+++ src/core/redis.py
@@ -3,4 +3,4 @@ from redis.asyncio import Redis
 from core.config import redis_settings
 
 
-redis = Redis(host=redis_settings.HOST, db=redis_settings.DB)
+redis = Redis(host=redis_settings.HOST, db=redis_settings.DB, port=redis_settings.PORT)
