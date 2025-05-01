507e2132a25eb4ee02361e1be9c48450a6cc4ecf
smplgdev <73465772+smplgdev@users.noreply.github.com>
2025-05-01 16:56:03 +0200

    Remove throttling from group

--- src/bot/handlers/private/__init__.py
+++ src/bot/handlers/private/__init__.py
@@ -2,10 +2,13 @@ from aiogram import F, Router
 from aiogram.enums import ChatType
 
 from bot.handlers.private import start
+from bot.middlewares.throttling import ThrottlingMiddleware
+from core.redis import redis
 
 
 private_router = Router()
 private_router.message.filter(F.chat.type == ChatType.PRIVATE)
+private_router.message.middleware(ThrottlingMiddleware(redis=redis))
 
 
 private_router.include_routers(
--- src/bot/main.py
+++ src/bot/main.py
@@ -1,14 +1,10 @@
 import asyncio
 
-from aiogram.methods import delete_webhook
-
 from bot.dependencies import bot, dp
 from bot.handlers.private import private_router
 from bot.handlers.group import group_router
 from bot.middlewares.db import DbSessionMiddleware
-from bot.middlewares.throttling import ThrottlingMiddleware
 
-from core.redis import redis
 from core.logging import setup_logging
 from db.base import sessionmaker
 
@@ -21,7 +17,6 @@ async def main():
         group_router
     )
     dp.update.middleware(DbSessionMiddleware(session_pool=sessionmaker))
-    dp.message.middleware(ThrottlingMiddleware(redis=redis))
 
     await bot.delete_webhook(drop_pending_updates=True)
     
